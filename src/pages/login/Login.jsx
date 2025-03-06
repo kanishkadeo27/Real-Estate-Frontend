5b570719f28a18d02d0ba579b5c8100eff482ee6
kanishkadeo27 <kanishkadeo92022@gmail.com>
2025-03-06 14:32:20 +0530

    register and login ui and functionality

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest.js";
 import "./login.scss";
 const Login = () => {
-  return <div className="login">Login</div>;
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const password = formData.get("password");
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
+      localStorage.setItem("user", JSON.stringify(res.data));
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <div className="wrapper">
+          <form onSubmit={handleSubmit}>
+            <h1>Welcome Back</h1>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              required
+              minLength={3}
+              maxLength={20}
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+            <button disabled={isLoading}>Login</button>
+            {error && <span className="error">{error.slice(0, 22)}</span>}
+            <Link to="/register" className="loginLink">
+              Don't have an account?
+            </Link>
+          </form>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <div className="wrapper">
+          <img src="/public/bg.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./register.scss";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest.js";
+
 const Register = () => {
-  return <div className="register">Register</div>;
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    try {
+      const res = await apiRequest.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      setError(error.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="register">
+      <div className="formContainer">
+        <div className="wrapper">
+          <form onSubmit={handleSubmit}>
+            <h1>Create an Account</h1>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+            />
+            <input type="text" name="email" id="email" placeholder="Email" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+            <button disabled={isLoading}>Register</button>
+            {error && <span className="error">{error.slice(0, 22)}</span>}
+            <Link to="/login" className="loginLink">
+              Already have an account?
+            </Link>
+          </form>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <div className="wrapper">
+          <img src="/public/bg.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
